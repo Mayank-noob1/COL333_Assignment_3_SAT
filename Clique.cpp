751b2f299647769d3d667e7ff2a80c28e41269d5
Mayank-noob1 <109759622+Mayank-noob1@users.noreply.github.com>
2023-10-09 18:44:21 +0530

    Update Clique.cpp
    Clause and offset correction

--- Clique.cpp
+++ Clique.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-typedef long long ll;
-
-
 
 string generate_edge_clause(int i,int j) const{
     string clause = "-"+to_string(i)+" "+"-"+to_string(j)+" 0";
@@ -19,7 +16,7 @@ string generate_clause(int i,int k,int K,int offset) const{
     }
     string clause1 = to_string(i)+" "+to_string(offset+k+(i-1)*K)+" 0";
     string clause2 = to_string(offset+k-1+(i-1)*K)+" "+to_string(offset+k+(i-1)*K)+" 0";
-    return clause;
+    return clause1+ clause2;
 }
 
 void generate_clauses_for_not_connected_vertices(vector<string> &clauses,vector<vector<int> > &edges){
@@ -43,36 +40,34 @@ void sort_vertices_according_to_degrees(vector<vector<int> > &edges){
                 return left.size() < right.size();});
 };
 
-void find_disjoint_cliques(int n,int m, int k1, int k2,vector<vector<int> > &edges,vector<string>& clauses){
-    for (int i=1;i<=n;i++){
-        string clause = generate_edge_clause(i,i+n);
-        clauses.push_back(clause);
-    }
-
-    // State : S[i,k] = S[i-1,k] | (S[i-1,k-1] & X[i])
-    // Map : S[i,k] = K*i + k
-    // Domain :  0 <= i <= n, 0 <= k <= K
-    int offset=2*n+1;
+void generate_clause_with_offset(vector<string> &clauses, int n,int k1,int offset){
     for(int i = 0;i <= n;i++){
         for(int k = 0;k<=k1;k++){
             string clause = generate_clause(i,k,k1,offset);
             clauses.push_back(clause);
         }
     }
-    offset += n*k1;
-    for(int i = 0;i <= n;i++){
-        for(int k = 0;k<=k2;k++){
-            string clause = generate_clause(i,k,k2,offset);
-            clauses.push_back(clause);
-        }
+}
+
+void find_disjoint_cliques(int n,int m, int k1, int k2,vector<vector<int> > &edges,vector<string>& clauses){
+    for (int i=1;i<=n;i++){
+        string clause = generate_edge_clause(i,i+n);
+        clauses.push_back(clause);
     }
+    // State : S[i,k] = S[i-1,k] | (S[i-1,k-1] & X[i])
+    // Map : S[i,k] = K*i + k
+    // Domain :  0 <= i <= n, 0 <= k <= K
+    int offset=2*n+1;
+    generate_clause_with_offset(clause,n,k1,offset);
+    offset += (n+1)*(k1+1);
+    generate_clause_with_offset(clause,n,k2,offset);
     return;
 };
 
 void find_maximal_clique(int n,int m, vector<vector<int> > &edges, vector<string>& clauses){
-
+    // Binary search on k
+    return;
 };
-
 
 int main(){
     ios::sync_with_stdio(0);
@@ -81,18 +76,14 @@ int main(){
     int n=0,m=0,k1=0,k2=0;cin>>n>>m>>k1>>k2;
     vector<vector<int> > edges;
 
-
     vector<string> clauses;
     generate_clauses_for_not_connected_vertices(clauses,edges);
     bool findMaximal = false;
-
-    // sort_vertices_according_to_degrees(edges);
     if (findMaximal){
         find_maximal_clique(n,m,edges,clauses);
     }
     else{
         find_disjoint_cliques(n,m,k1,k2,edges,clauses);
     }
-
     return 0;
 }
